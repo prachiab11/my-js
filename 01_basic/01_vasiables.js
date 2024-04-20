82fd5d1b5fe0996a4e3ab778e03d7fe5c6bc5fa7
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-04-20 14:53:14 +0000

    1st file

---
+++ 01_basic/01_vasiables.js
@@ -0,0 +1,14 @@
+const accountId= 145634
+let accountEmail = "prachi@google.com"
+var accountPassword="123456"
+accountCity="Pune"
+let accountState;
+
+//SaccountId=2 //not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword ="212121"
+accountCity = "Jaipur"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, acocuntState])
