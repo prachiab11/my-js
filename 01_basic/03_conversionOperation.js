e79c1ec240a24a876709bcbeb19504b712cc2959
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-04-20 14:52:19 +0000

    converion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "apple"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let isLoggedIn =1
+let booleadIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof(booleadIsLoggedIn))
+//console.log(booleadIsLoggedIn)
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof(stringNumber))
