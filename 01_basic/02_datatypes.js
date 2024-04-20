600bb87bb2d4b61696a7c02f42c23fb6854077e3
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-04-20 14:29:52 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+"use script";//treat all js code as newer version
+// alert(3+3)  //we are using node js not browser
+
+console.log( 3 + 3)
+console.log("hi")// code readability should be high
+
+let name = "prachi"
+let age =25
+let isLoggedIn = false
+
+console.log(typeof "hitesh")
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
