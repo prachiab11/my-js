64fe94080c4f169d5095ba39163693230f335a06
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-04-20 18:26:34 +0000

    summary done

---
+++ 01_basic/datatype_summary_memory.js
@@ -0,0 +1,19 @@
+let myYoutubeName = "hiteshchodharydotcom"
+
+let anothername =  myYoutubeName
+anothername= "chaiaurcode"
+
+// console.log(myYoutubeName)
+// console.log(anothername)
+
+let user={
+    email:"iam@gmail.com",
+    upi:'user@ybl'
+}
+
+let userTwo=user;
+
+userTwo.email = "user@gmail.com"
+
+console.log(user.email)
+console.log(userTwo.email)
