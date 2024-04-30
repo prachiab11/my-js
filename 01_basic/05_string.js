12f9ea822208f0fdd90460668a37ecd826d61b73
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-04-30 09:59:26 +0000

    string added

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,6 +1,26 @@
 const name="prachi"
 const repoCount = 50
 
-console.log(name + repoCount + " value")
+// console.log(name + repoCount + " value")
 
-console.log(`hello my name is  ${name} and value is ${repoCount}`)
+// console.log(`hello my name is  ${name} and value is ${repoCount}`)
+
+const gamename = new String('Prachi');
+
+// console.log(gamename[0])
+// console.log(gamename.__proto__)
+
+// console.log(gamename.length)
+// console.log(gamename.toUpperCase())
+
+// console.log(gamename.charAt(4))
+
+const newString= gamename.substring(0,4)
+console.log(newString)
+
+const newStringOne= gamename.slice(-8,2)
+console.log(newStringOne)
+
+const newStringTwo= "   Prachi   "
+console.log(newStringTwo)
+console.log(newStringTwo.trim())
