0e2e3ce4cd89f4afa7842dbbb841f645038aec43
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-05-09 15:06:17 +0000

    completed basics

---
+++ 01_basic/07_date&time.js
@@ -0,0 +1,22 @@
+let myDate= new Date();
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+ let createdDate = new Date(2024, 0, 23)
+// console.log(createdDate.toDateString())
+
+let timeStamp= Date.now();
+console.log(timeStamp)
+console.log(createdDate.getTime());
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
