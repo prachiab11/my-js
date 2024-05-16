c5c2f69f7a80dba78a49afdf41c6c75f2473805d
prachiab11 <74653344+prachiab11@users.noreply.github.com>
2024-05-16 14:52:22 +0000

    json started

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -41,9 +41,19 @@ const users = [
 ]
 
 //console.log(users[1].email)
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+coursename: "js in hindi",
+price : "999",
+courseInstructor : "hitesh"
+}
+//console.log(course.courseInstructor)
+
+const {courseInstructor : instructor}= course
+console.log(instructor)
